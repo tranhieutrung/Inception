2d9be13a36945b2c50b1b0be44d1b18b0853fc1e
tranhieutrung <tranhieutrung@outlook.com>
2025-06-16 15:21:21 +0300

    Add notes into mariadb-server.conf and nginx's entrypoint.sh.

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 set -e
 
-openssl req -x509 -days 365 -newkey rsa:2048 -nodes \
+# create a SSL (self-signed certificate)
+#	-x509: create X.509 certificate.
+#	-days 365: valid for 365 days.
+#	-newkey rsa:2048: generate a new 2048-bit RSA key.
+#	-nodes: do not encrypt the private key.
+#	-keyout: output file for private key.
+#	-out: output file for certificate.
+#	-subj: set the certificate subject (Common Name) from $DOMAIN_NAME.
+openssl req -x509 \
+			-days 365 \
+			-newkey rsa:2048 \
+			-nodes \
 			-keyout /etc/nginx/ssl/cert.key \
 			-out /etc/nginx/ssl/cert.crt \
 			-subj "/CN=$DOMAIN_NAME"
 
+# Set file permissions
+# Private key readable/writable only by owner (600)
 chmod 600 /etc/nginx/ssl/cert.key
+# Certificate readable by everyone (644)
 chmod 644 /etc/nginx/ssl/cert.crt
 
 exec nginx -g "daemon off;"
